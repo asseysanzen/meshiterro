560ba5d7ac1444f74a882860fff88e67ebf48f6c
asseysanzen <asseysanzen@gmail.com>
2020-04-16 16:06:46 +0900

    [finish] 6-6

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
